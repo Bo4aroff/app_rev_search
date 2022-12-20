02f4e37e4d5607f50047525a61f2c05475f36ee5
Bo4aroff <104056673+Bo4aroff@users.noreply.github.com>
2022-12-20 23:49:28 +0300

    Update scrap_2.py

--- scrap_2.py
+++ scrap_2.py
@@ -35,8 +35,8 @@ a_df2 = a_df2.where(pd.notnull(a_df2), None)
 
 a_df2['review_date'] = a_df2['review_date'].dt.strftime('%m/%d/%Y')
 a_df2['date'] = pd.to_datetime(a_df2['review_date']).dt.normalize()
-a_df2['month'] = a_df2['date'].dt.month
-a_df2['year'] = a_df2['date'].dt.year
+a_df2['month'] = a_df2['review_date'].dt.month
+a_df2['year'] = a_df2['review_date'].dt.year
 a_df2.loc[a_df2['rating'] < 4, 'рейтинг'] = 'Отрицательный'
 a_df2.loc[a_df2['rating'] >= 4, 'рейтинг'] = 'Положительный'
 a_df2.loc[a_df2['rating'] > 0, 'value'] = 1
