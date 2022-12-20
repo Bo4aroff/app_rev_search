4b2acd7dfb4349fe2298d6aae0bb6af2fb1a1023
Bo4aroff <104056673+Bo4aroff@users.noreply.github.com>
2022-12-20 23:17:58 +0300

    Update scrap_2.py

--- scrap_2.py
+++ scrap_2.py
@@ -32,7 +32,7 @@ a_df2.rename(columns= {'review': 'review_description','userName': 'user_name', '
 a_df2 = a_df2.where(pd.notnull(a_df2), None)
 
 # a_df2['date'] = pd.to_datetime(a_df2['review_date']).dt.floor('D')
-a_df2['date'] = pd.to_datetime(a_df2['review_date']).dt.normalize()
+a_df2['date'] = pd.to_datetime(a_df2['review_date']).dt.date
 a_df2['month'] = a_df2['date'].dt.month
 a_df2['year'] = a_df2['date'].dt.year
 a_df2.loc[a_df2['rating'] < 4, 'рейтинг'] = 'Отрицательный'
