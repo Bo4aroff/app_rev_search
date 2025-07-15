93db6947cc40549e74fd571b6ded35010f9fd6f9
Bo4aroff <104056673+Bo4aroff@users.noreply.github.com>
2025-07-15 21:57:57 +0300

    Update init.py

--- init.py
+++ init.py
@@ -33,14 +33,14 @@ g_df2.insert(loc=0, column='source', value='Google Play')
 g_df2.insert(loc=3, column='review_title', value=None)
 
 
-a_reviews = AppStore('ru', 'storemed', '6736902949')
+a_reviews = AppStore('us', 'storemed', '6736902949')
 a_reviews.review()
 
 
 a_df = pd.DataFrame(np.array(a_reviews.reviews), columns=['review'])
 a_df2_ = a_df.join(pd.DataFrame(a_df.pop('review').tolist()))
 
-#a_df2_.drop(columns={'isEdited'}, inplace = True)
+a_df2_.drop(columns={'isEdited'}, inplace = True)
 a_df2_.insert(loc=0, column='source', value='App Store')
 a_df2_['developer_response_date'] = None
 a_df2_['thumbs_up'] = None
